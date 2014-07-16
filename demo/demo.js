c253251280a36c024b7273143547369bb9d218c4
Matt Pileggi <Matt.Pileggi@marklogic.com>
2014-07-16 11:52:24 -0400

    pass map options to new maps

--- demo/demo.js
+++ demo/demo.js
@@ -2,22 +2,24 @@ var demoApp = angular.module('demoApp',['ml-utils','hljs']);
 
 demoApp.controller('DemoCtrl', function() {
   var demoControl = this;
-  demoControl.mapNames = []; // store map names that we know
+  demoControl.maps = {}; // store map names that we know
   demoControl.nextMap = {};
   demoControl.hideMaps = false;
+  demoControl.hasOtherMaps = false;
 
   this.addMap = function() {
     if (!demoControl.nextMap.name) {
       alert('Please give your map a name');
       return;
     }
-    if (demoControl.nextMap.name === 'main' || demoControl.mapNames.indexOf(demoControl.nextMap.name) > -1) {
+    if (demoControl.nextMap.name === 'main' || demoControl.maps.hasOwnProperty(demoControl.nextMap.name)) {
       alert('A map by that name already exists');
       demoControl.nextMap.name = '';
       return;
     }
+    demoControl.hasOtherMaps = true;
     console.log('adding map',demoControl.nextMap.name);
-    demoControl.mapNames.push(demoControl.nextMap.name);
+    demoControl.maps[demoControl.nextMap.name] = { controls: demoControl.nextMap.hideCtrl ? 'off' : 'on' }
     demoControl.nextMap = {};
   }
 })
